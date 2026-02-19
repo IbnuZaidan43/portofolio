ae0cdff7eb7cc14d2a23e4e8f98a65c73d200c3a
IbnuZaidan43 <akbaribnu031@gmail.com>
2026-02-19 21:53:29 +0700

    first slide

--- portofolio-ibnu/app/page.tsx
+++ portofolio-ibnu/app/page.tsx
@@ -2,24 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   const skill = [
-        { id: 1, src: "/logo/html.svg", width: 73, height: 103},
-        { id: 2, src: "/logo/css.svg", width: 72, height: 103},
-        { id: 3, src: "/logo/js.svg", width: 72, height: 103},
-        { id: 4, src: "/logo/c++.svg", width: 93, height: 93},
-        { id: 5, src: "/logo/c.svg", width: 103, height: 103},
-        { id: 6, src: "/logo/java.svg", width: 77, height: 103},
-        { id: 7, src: "/logo/react.svg", width: 103, height: 93},
-        { id: 8, src: "/logo/nextjs.svg", width: 103, height: 84},
-        { id: 9, src: "/logo/tailwind.svg", width: 100, height: 77},
-        { id: 10, src: "/logo/laravel.svg", width: 103, height: 83},
-        { id: 11, src: "/logo/ci.svg", width: 103, height: 93}
-    ];
+    { id: 1, src: "/logo/html.svg", width: 73, height: 103},
+    { id: 2, src: "/logo/css.svg", width: 72, height: 103},
+    { id: 3, src: "/logo/js.svg", width: 72, height: 103},
+    { id: 4, src: "/logo/c++.svg", width: 93, height: 93},
+    { id: 5, src: "/logo/c.svg", width: 103, height: 103},
+    { id: 6, src: "/logo/java.svg", width: 77, height: 103},
+    { id: 7, src: "/logo/react.svg", width: 103, height: 93},
+    { id: 8, src: "/logo/nextjs.svg", width: 103, height: 84},
+    { id: 9, src: "/logo/tailwind.svg", width: 100, height: 77},
+    { id: 10, src: "/logo/laravel.svg", width: 103, height: 83},
+    { id: 11, src: "/logo/ci.svg", width: 103, height: 93}
+  ];
 
   const icon = [
-        { id: 1, src: "/icons/wa.svg", width: 60, height: 60, objective: ""},
-        { id: 2, src: "/icons/ig.svg", width: 60, height: 60, objective: ""},
-        { id: 3, src: "/icons/linkedin.svg", width: 60, height: 60, objective: ""}
-    ];
+    { id: 1, src: "/icons/wa.svg", width: 60, height: 60, objective: "https://wa.me/6282325299550"},
+    { id: 2, src: "/icons/ig.svg", width: 60, height: 60, objective: "https://www.instagram.com/ibnu_zaidanj"},
+    { id: 3, src: "/icons/linkedin.svg", width: 60, height: 60, objective: "https://www.linkedin.com/in/ibnuzaidan"}
+  ];
+
+  const project = [
+    { id: 1, foto: "/projectProfile/nexus.png", objective: "/nexus"},
+    { id: 2, foto: "/projectProfile/himasela.png", objective: "/himasela"}
+  ]
 
   return (
     <main className="bg-dark">
@@ -33,7 +38,8 @@ export default function Home() {
             className="h-[70dvh] w-auto object-contain"
             />
         </div>
-        <div className="min-h-screen w-full flex flex-col gap-4 justify-center pr-24">
+
+        <div className="min-h-screen w-full flex flex-col gap-4 justify-center pr-24 bg-[radial-gradient(circle_at_right,_var(--light)_0%,_var(--dark)_35%)]">
           <div className="flex flex-col">
             <span className="text-[42px] font-semibold">About Me</span>
             <p className="text-[20px] text-justify leading-tight">I'm driven by the challenge of turning complex ideas into digital products that are intuitive, efficient, and relevant. My interest lies in how code, design, and insight can work together to solve real problems, not only from a technical perspective but also in ways that are purposeful and impactful for users. I enjoy exploring how users interact with systems, how data informs direction, and how small design decisions can shape large outcomes. With a technical mindset and a creative edge, I'm constantly learning new tools, experimenting with ideas, and seeking opportunities to contribute to projects that are both functional and forward-thinking.</p>
@@ -57,7 +63,7 @@ export default function Home() {
                 <Image
                   key={item.id} src={item.src}
                   width={item.width} height={item.height}
-                  alt="logo"
+                  alt="logo" 
                 />
               ))}
             </div>
@@ -67,14 +73,35 @@ export default function Home() {
             <span className="text-[42px] font-semibold">Contacts</span>
             <div className="flex flex-wrap gap-x-8 gap-y-4 w-full">
               {icon.map((item) => (
-                <Image
-                  key={item.id} src={item.src}
-                  width={item.width} height={item.height}
-                  alt="icon"
-                />
+                <a key={item.id} href={item.objective} target="_blank">
+                  <Image
+                    width={item.width} height={item.height}
+                    src={item.src} alt="icon"
+                    />
+                </a>
               ))}
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="py-8 px-12">
+        <span className="text-[48px] font-semibold">Featured Project</span>
+        <div className="flex flex-wrap gap-8">
+          {project.map((item) => (
+            <a key={item.id} href={item.objective}>
+              {/* div buat background abu gitu */}
+              <div className="">
+                {/* div buat image */}
+                <div>
+                  <Image
+                    width={128} height={128}
+                    src={item.foto} alt="project"
+                  />
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </main>
