a9a77bd0851607c18ca75cc14c27d2c3ed7a27df
IbnuZaidan43 <akbaribnu031@gmail.com>
2026-02-22 14:35:50 +0700

    a half responsive main page

--- portofolio-ibnu/app/page.tsx
+++ portofolio-ibnu/app/page.tsx
@@ -2,29 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   const skill = [
-    { id: 1, src: "/logo/html.svg", width: 73, height: 103},
-    { id: 2, src: "/logo/css.svg", width: 72, height: 103},
-    { id: 3, src: "/logo/js.svg", width: 72, height: 103},
-    { id: 4, src: "/logo/c++.svg", width: 93, height: 93},
-    { id: 5, src: "/logo/c.svg", width: 103, height: 103},
-    { id: 6, src: "/logo/java.svg", width: 77, height: 103},
-    { id: 7, src: "/logo/react.svg", width: 103, height: 93},
-    { id: 8, src: "/logo/nextjs.svg", width: 103, height: 84},
-    { id: 9, src: "/logo/tailwind.svg", width: 100, height: 77},
-    { id: 10, src: "/logo/laravel.svg", width: 103, height: 83},
-    { id: 11, src: "/logo/ci.svg", width: 103, height: 93}
+    { id: 1, src: "/logo/html.svg", xlWidth: 73, xlHeight: 103, size: "xl:w-[73px] xl:h-[103px] lg:w-[59px] lg:h-[83px]"},
+    { id: 2, src: "/logo/css.svg",  xlWidth: 72, xlHeight: 103, size: "xl:w-[72px] xl:h-[103px] lg:w-[58px] lg:h-[83px]"},
+    { id: 3, src: "/logo/js.svg",   xlWidth: 72, xlHeight: 103, size: "xl:w-[72px] xl:h-[103px] lg:w-[58px] lg:h-[83px]"},
+    { id: 4, src: "/logo/c++.svg",  xlWidth: 93, xlHeight:  93, size: "xl:w-[93px] xl:h-[93px] lg:w-[75px]  lg:h-[75px]"},
+    { id: 5, src: "/logo/c.svg",   xlWidth: 103,xlHeight: 103, size: "xl:w-[103px] xl:h-[103px] lg:w-[83px] lg:h-[83px]"},
+    { id: 6, src: "/logo/java.svg", xlWidth: 77, xlHeight: 103, size: "xl:w-[77px] xl:h-[103px] lg:w-[62px] lg:h-[83px]"},
+    { id: 7, src: "/logo/react.svg",xlWidth: 103,xlHeight:  93, size: "xl:w-[103px] xl:h-[93px] lg:w-[83px] lg:h-[75px]"},
+    { id: 8, src:"/logo/nextjs.svg",xlWidth: 103,xlHeight:  84, size: "xl:w-[103px] xl:h-[84px] lg:w-[83px] lg:h-[68px]"},
+    { id:9,src:"/logo/tailwind.svg",xlWidth: 100,xlHeight:  77, size: "xl:w-[100px] xl:h-[77px] lg:w-[80px] lg:h-[62px]"},
+    { id:10,src:"/logo/laravel.svg",xlWidth: 103,xlHeight:  83, size: "xl:w-[103px] xl:h-[83px] lg:w-[83px] lg:h-[67px]"},
+    { id: 11, src: "/logo/ci.svg",  xlWidth: 103,xlHeight:  93, size: "xl:w-[103px] xl:h-[93px] lg:w-[83px] lg:h-[75px]"}
   ];
 
   const icon = [
-    { id: 1, src: "/icons/wa.svg", width: 60, height: 60, objective: "https://wa.me/6282325299550"},
-    { id: 2, src: "/icons/ig.svg", width: 60, height: 60, objective: "https://www.instagram.com/ibnu_zaidanj"},
-    { id: 3, src: "/icons/linkedin.svg", width: 60, height: 60, objective: "https://www.linkedin.com/in/ibnuzaidan"}
+    { id: 1, src: "/icons/wa.svg", width: 40, height: 40, objective: "https://wa.me/6282325299550"},
+    { id: 2, src: "/icons/ig.svg", width: 40, height: 40, objective: "https://www.instagram.com/ibnu_zaidanj"},
+    { id: 3, src: "/icons/linkedin.svg", width: 40, height: 40, objective: "https://www.linkedin.com/in/ibnuzaidan"}
   ];
 
   const project = [
-    { id: 1, foto: "/projectProfile/nexus.png", objective: "/nexus", title: "Nexus Financial App", position: "fullstack developer"},
-    { id: 2, foto: "/projectProfile/himasela.png", objective: "/himasela", title: "Organization Website HIMASELA", position: "fullstack developer"},
-    { id: 3, foto: "/projectProfile/safeena.png", objective: "/safeena", title: "Company Profile Webiste", position: "frontend developer"}
+    { id: 1, foto: "/projectProfile/safeena.png", objective: "/safeena", title: "Company Profile Website", position: "frontend developer"},
+    { id: 2, foto: "/projectProfile/nexus.png", objective: "/nexus", title: "Nexus Financial App", position: "fullstack developer"},
+    { id: 3, foto: "/projectProfile/himasela.png", objective: "/himasela", title: "Organization Website HIMASELA", position: "fullstack developer"},
   ]
 
   const positionColor = (posisi: string) => {
@@ -43,50 +43,72 @@ export default function Home() {
 
   return (
     <main className="bg-dark">
-      <div className="grid grid-cols-[40%_60%]">
-        <div className="min-h-screen w-full flex flex-col gap-0 items-center justify-center bg-[radial-gradient(circle_at_center,_var(--light)_0%,_var(--dark)_60%)]">
-          <span className="text-[24px] leading-tight">Hello, I am</span>
-          <span className="text-[60px] leading-tight font-bold">Ibnu Zaidan Akbar</span>
+      <div className="grid lg:grid-cols-[35%_65%]">
+        <div className="min-h-screen w-full flex flex-col items-center justify-center lg:bg-[radial-gradient(circle_at_center,_var(--light)_0%,_var(--dark)_50%)]">
+          <span className="lg:text-[20px] xl:text-[24px] leading-tight">Hello, I am</span>
+          <span className="lg:text-[36px] xl:text-[48px] leading-tight font-bold">Ibnu Zaidan Akbar</span>
           <Image
             src="/foto.png" alt="foto"
-            width={1290} height={1720} priority
-            className="h-[70dvh] w-auto object-contain"
+            width={1290} height={540} priority
+            className="h-[70dvh] mlg:w-[844px] lg:h-[420px] xl:w-[1290px] xl:h-[540px] w-auto object-contain"
             />
         </div>
 
-        <div className="min-h-screen w-full flex flex-col gap-4 justify-center pr-24 bg-[radial-gradient(circle_at_right,_var(--light)_0%,_var(--dark)_35%)]">
+        <div className="min-h-screen w-full flex flex-col gap-4 justify-center lg:pr-12 xl:pr-20 xl:bg-[radial-gradient(circle_at_right,_var(--light)_0%,_var(--dark)_35%)]">
           <div className="flex flex-col">
-            <span className="text-[42px] font-semibold">About Me</span>
-            <p className="text-[20px] text-justify leading-tight">I'm driven by the challenge of turning complex ideas into digital products that are intuitive, efficient, and relevant. My interest lies in how code, design, and insight can work together to solve real problems, not only from a technical perspective but also in ways that are purposeful and impactful for users. I enjoy exploring how users interact with systems, how data informs direction, and how small design decisions can shape large outcomes. With a technical mindset and a creative edge, I'm constantly learning new tools, experimenting with ideas, and seeking opportunities to contribute to projects that are both functional and forward-thinking.</p>
+            <span className="lg:text-[28px] xl:text-[42px] font-semibold">About Me</span>
+            <p className="lg:text-[16px] xl:text-[20px] text-justify leading-tight">I'm driven by the challenge of turning complex ideas into digital products that are intuitive, efficient, and relevant. My interest lies in how code, design, and insight can work together to solve real problems, not only from a technical perspective but also in ways that are purposeful and impactful for users. I enjoy exploring how users interact with systems, how data informs direction, and how small design decisions can shape large outcomes. With a technical mindset and a creative edge, I'm constantly learning new tools, experimenting with ideas, and seeking opportunities to contribute to projects that are both functional and forward-thinking.</p>
           </div>
 
           <div className="flex justify-between">
             <div className="flex flex-col">
-              <span className="text-[42px] font-semibold">Education</span>
-              <p className="text-[24px] leading-tight">● 2024 - Present<br/>Padjadjaran University, Sumedang</p>
+              <span className="lg:text-[28px] xl:text-[42px] font-semibold">Education</span>
+              <p className="lg:text-[16px] xl:text-[24px] leading-tight">● 2024 - Present<br/>Padjadjaran University, Sumedang</p>
             </div>
             <div className="flex flex-col">
-              <span className="text-[42px] font-semibold">Internship Experience</span>
-              <p className="text-[24px] leading-tight">● Nov 2025 - Jan 2026<br/>Safeena Academy - Frontend Developer</p>
+              <span className="lg:text-[28px] xl:text-[42px] font-semibold">Internship Experience</span>
+              <p className="lg:text-[16px] xl:text-[24px] leading-tight">● Nov 2025 - Jan 2026<br/>Safeena Academy - Frontend Developer</p>
             </div>
           </div>
 
           <div className="flex flex-col">
-            <span className="text-[42px] font-semibold">Skills</span>
+            <span className="lg:text-[28px] xl:text-[42px] font-semibold">Skills</span>
             <div className="flex flex-wrap gap-y-4 gap-x-8 w-full">
               {skill.map((item) => (
                 <Image
                   key={item.id} src={item.src}
-                  width={item.width} height={item.height}
-                  alt="logo" 
+                  width={item.xlWidth} height={item.xlHeight}
+                  alt="logo" className={item.size}
                 />
               ))}
             </div>
           </div>
+        </div>
+      </div>
 
-          <div>
-            <span className="text-[42px] font-semibold">Contacts</span>
-            <div className="flex flex-wrap gap-x-8 gap-y-4 w-full">
+      <div className="flex flex-col lg:py-4 lg:px-4 xl:py-12 xl:px-6">
+        <span className="lg:text-[36px] xl:text-[48px] font-semibold lg:pb-4 text-center">Featured Project</span>
+        <div className="flex flex-wrap lg:gap-4 xl:gap-8 justify-center">
+          {project.map((item) => (
+            <a key={item.id} href={item.objective}>
+              <div className="bg-[#4E4F51] lg:w-[390px] xl:w-[440px] h-fit rounded-2xl flex flex-col" >
+                <div>
+                  <Image
+                    width={400} height={240}
+                    src={item.foto} alt="project"
+                    className="rounded-2xl lg:w-[350px] lg:h-[170px]"
+                  />
+                </div>
+                <span className={"w-fit lg:text-[12px] text-white/90 font-semibold mt-2 mx-4 py-1 px-4 inline-block rounded-full " + positionColor(item.position)}>{item.position}</span>
+                <span className="lg:text-[20px] text-white font-bold py-2 px-4 leading-tight">{item.title}</span>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col lg:p-12 lg:gap-4">
+            <span className="lg:text-[36px] font-semibold text-center">Contacts</span>
+            <div className="flex flex-wrap lg:gap-x-8 lg:gap-y-4 w-full justify-center">
               {icon.map((item) => (
                 <a key={item.id} href={item.objective} target="_blank">
                   <Image
@@ -97,31 +119,6 @@ export default function Home() {
               ))}
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col py-8 px-12">
-        <span className="text-[48px] font-semibold pb-4">Featured Project</span>
-        <div className="flex flex-wrap justify-between">
-          {project.map((item) => (
-            <a key={item.id} href={item.objective} target="_blank">
-              {/* div buat background abu gitu */}
-              <div className="bg-[#4E4F51] w-[570px] h-fit rounded-2xl flex flex-col" >
-                {/* div buat image */}
-                <div>
-                  <Image
-                    width={512} height={300}
-                    src={item.foto} alt="project"
-                    className="rounded-2xl"
-                  />
-                </div>
-                <span className={"w-fit text-[16px] text-white/90 font-semibold mt-4 mx-4 py-2 px-4 inline-block rounded-full " + positionColor(item.position)}>{item.position}</span>
-                <span className="text-[28px] text-white font-bold py-2 px-4">{item.title}</span>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
     </main>
   );
 }
--- portofolio-ibnu/app/safeena/page.tsx
+++ portofolio-ibnu/app/safeena/page.tsx
@@ -1,5 +1,44 @@
+import Image from "next/image";
+
 export default function Safeena() {
-    <main className="bg-dark">
-        
-    </main>
+    const foto = [
+        { id: 1, src: "/projectProfile/himasela.png"},
+        { id: 2, src: "/assets/h1.png"},
+        { id: 3, src: "/assets/h2.png"},
+        { id: 4, src: "/assets/h3.png"},
+        { id: 5, src: "/assets/h4.png"},
+        { id: 6, src: "/assets/h5.png"},
+        { id: 7, src: "/assets/h6.png"}
+    ];
+    
+    return (
+        <main className="bg-dark">
+            <div className="min-h-screen bg-[radial-gradient(circle_at_top_right,var(--light)_0%,transparent_30%),radial-gradient(circle_at_bottom_left,var(--light)_0%,var(--dark)_30%)]">
+                <div className="grid grid-cols-[15%_85%] p-6">
+                    <div className="p-10 bg-[#E0E4E7]/70 rounded-full">
+                        <Image
+                            src="/assets/lh.svg" alt="logo himasela"
+                            width={216} height={180}
+                        />
+                    </div>
+                    <div className="flex flex-col p-8 justify-center">
+                        <span className="text-[42px] font-bold">Website Department Keilmiahan Himasela Unsoed</span>
+                        <p className="text-[28px] leading-none">An information management website platform for the "land resource planning student association" designed to integrate work program documentation, department progress, and digital asset management through Google Drive-based storage synchronization.</p>
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-center gap-y-2">
+                    {foto.map((item) => (
+                        <Image
+                        key={item.id}
+                            src={item.src} alt="dokumentasi"
+                            width={630} height={332} className="px-2 py-1 rounded-3xl"
+                            />
+                        ))}
+                </div>
+                <div className="max-w h-fit flex p-8 justify-center align-center">
+                    <a className="text-[24px]" href="https://www.keilmiahanhimasela.space/" target="_blank">View Project 👁</a>
+                </div>
+            </div>
+        </main>
+    )
 }
