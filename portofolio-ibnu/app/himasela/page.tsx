bf3d5b217849d180a06eba2fdd49d13a5aee54f5
IbnuZaidan43 <akbaribnu031@gmail.com>
2026-02-22 07:07:29 +0700

    add 1 project page

--- portofolio-ibnu/app/himasela/page.tsx
+++ portofolio-ibnu/app/himasela/page.tsx
@@ -14,19 +14,19 @@ export default function Himasela() {
     return (
         <main className="bg-dark">
             <div className="min-h-screen bg-[radial-gradient(circle_at_top_right,var(--light)_0%,transparent_30%),radial-gradient(circle_at_bottom_left,var(--light)_0%,var(--dark)_30%)]">
-                <div className="flex pl-6 p-4">
+                <div className="grid grid-cols-[15%_85%] p-6">
                     <div className="p-10 bg-[#E0E4E7]/70 rounded-full">
                         <Image
                             src="/assets/lh.svg" alt="logo himasela"
                             width={216} height={180}
                         />
                     </div>
-                    <div className="flex flex-col">
-                        <span className="text-[36px] font-bold">Website Departemen Keilmiahan Himasela Unsoed</span>
-                        <p></p>
+                    <div className="flex flex-col p-8 justify-center">
+                        <span className="text-[42px] font-bold">Website Department Keilmiahan Himasela Unsoed</span>
+                        <p className="text-[28px] leading-none">An information management website platform for the "land resource planning student association" designed to integrate work program documentation, department progress, and digital asset management through Google Drive-based storage synchronization.</p>
                     </div>
                 </div>
-                <div className="flex flex-wrap justify-center">
+                <div className="flex flex-wrap justify-center gap-y-2">
                     {foto.map((item) => (
                         <Image
                         key={item.id}
@@ -34,6 +34,9 @@ export default function Himasela() {
                             width={630} height={332} className="px-2 py-1 rounded-3xl"
                             />
                         ))}
+                </div>
+                <div className="max-w h-fit flex p-8 justify-center align-center">
+                    <a className="text-[24px]" href="https://www.keilmiahanhimasela.space/" target="_blank">View Project 👁</a>
                 </div>
             </div>
         </main>
