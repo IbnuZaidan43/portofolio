33a876a3189d029e4fe68c6a827b1f6871094912
IbnuZaidan43 <akbaribnu031@gmail.com>
2026-02-26 23:51:10 +0700

    maybe its done?

--- portofolio-ibnu/app/nexus/page.tsx
+++ portofolio-ibnu/app/nexus/page.tsx
@@ -6,12 +6,13 @@ import Image from "next/image";
 export default function Nexus() {
     const [isExpanded, setIsExpanded] = useState(false);
     const foto = [
-        { id: 1, src: "/assets/s1.png"},
-        { id: 2, src: "/assets/s2.png"},
-        { id: 3, src: "/assets/s3.png"},
-        { id: 4, src: "/assets/s4.png"},
-        { id: 5, src: "/assets/s5.png"},
-        { id: 6, src: "/assets/s6.png"}
+        { id: 1, src: "/assets/n1.png"},
+        { id: 2, src: "/assets/n2.png"},
+        { id: 3, src: "/assets/n3.png"},
+        { id: 4, src: "/assets/n4.png"},
+        { id: 5, src: "/assets/n5.png"},
+        { id: 6, src: "/assets/n6.png"},
+        { id: 7, src: "/assets/n7.png"}
     ];
     const allFoto = foto.slice(0, 6);
     
@@ -19,15 +20,15 @@ export default function Nexus() {
             <main className="bg-dark">
                 <div className="min-h-screen bg-[radial-gradient(circle_at_top_right,var(--light)_0%,transparent_30%),radial-gradient(circle_at_bottom_left,var(--light)_0%,var(--dark)_30%)]">
                     <div className="flex flex-col md:grid md:grid-cols-[20%_80%] justify-center items-center py-4 lg:py-8 md:px-12 lg:px-12 xl:px-20">
-                        <div className="p-4 md:p-6 lg:p-8 xl:p-10 bg-[#E0E4E7]/70 rounded-full justify-center items-center">
+                        <div className="w-fit md:ml-auto lg:mr-4 2xl:mr-12 p-6 lg:p-8 xl:p-12 bg-[#E0E4E7]/70 rounded-full justify-center items-center">
                             <Image
-                                src="/assets/ls.svg" alt="logo safeena"
-                                width={360} height={310}
-                                className="w-[150px] h-[150px] md:w-[550px] md:h-[90px] lg:w-[500px] lg:h-[120px] xl:w-[450px] xl:h-[170px]"
+                                src="/assets/ln.svg" alt="logo nexus"
+                                width={100} height={100}
+                                className="w-[70px] h-[70px] md:w-[90px] md:h-[90px] lg:w-[100px] lg:h-[100px] xl:w-[120px] xl:h-[120px]"
                             />
                         </div>
                         <div className="flex flex-col gap-4 md:gap-2 p-8 justify-center">
-                            <span className="text-[20px] md:text-[28px] lg:text-[32px] xl:text-[42px] text-center md:text-start font-bold leading-none">Website Company Profile for Safeena Academy</span>
+                            <span className="text-[20px] md:text-[28px] lg:text-[32px] xl:text-[42px] text-center md:text-start font-bold leading-none">Native-App for financial Managemment</span>
                             <p className="text-[16px] lg:text-[20px] xl:text-[28px] text-justify leading-none">A company profile website that prioritizes access to information to expand market reach. Integrating interactive visual elements with structured information, it ensures the effective presentation of service data and company objectives. It features a registration form that automatically sends to the company email address.</p>
                         </div>
                     </div>
@@ -41,7 +42,7 @@ export default function Nexus() {
                         ))}
     
                         {!isExpanded && (
-                            <div className="w-full flex justify-center lg:hidden">
+                            <div className="w-full flex justify-center lg:hidden mt-4">
                                 <button onClick={() => setIsExpanded(true)} className="flex flex-col justify-center items-center gap-2 text-white hover:text-blue-400 transition-colors group">
                                     <Image
                                         src="/icons/expand.svg" alt="expand"
@@ -53,7 +54,7 @@ export default function Nexus() {
                         )}
                     </div>
                     <div className="max-w h-fit flex p-8 justify-center align-center">
-                        <a className="text-[16px] md:text-[20px] xl:text-[24px]" href="#" target="_blank">View Project 👁</a>
+                        <a className="text-[16px] md:text-[20px] xl:text-[24px]" href="https://nexus-financial-steel.vercel.app/" target="_blank">View Project 👁</a>
                     </div>
                 </div>
             </main>
